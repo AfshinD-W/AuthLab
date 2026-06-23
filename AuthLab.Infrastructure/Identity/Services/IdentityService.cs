970be441490664334b2a9b8bb72add7c6e123d3c
jafari <afshinaa705@gmail.com>
2026-06-23 10:19:01 +0330

    Develop first CreateUserAsync template in identityService

--- AuthLab.Infrastructure/Identity/Services/IdentityService.cs
+++ AuthLab.Infrastructure/Identity/Services/IdentityService.cs
@@ -1,4 +1,5 @@
-﻿using AuthLab.Infrastructure.Identity.Entities;
+﻿using AuthLab.Application.DTO;
+using AuthLab.Infrastructure.Identity.Entities;
 using Microsoft.AspNetCore.Identity;
 
 namespace AuthLab.Infrastructure.Identity.Services
@@ -10,6 +11,31 @@ namespace AuthLab.Infrastructure.Identity.Services
         public IdentityService(UserManager<User> userManager)
         {
             _userManager = userManager;
+        }
+
+        public async Task<UserResponseDTO> CreateUserAsync(UserRequestDTO requestDTO)
+        {
+            User user = new()
+            {
+                UserName = requestDTO.UserName,
+                Email = requestDTO.Email,
+                PhoneNumber = requestDTO.PhoneNumber,
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, requestDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = string.Join(", ", result.Errors.Select(e => e.Description));
+                throw new Exception(errors);
+            }
+
+            return new UserResponseDTO()
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+            };
         }
     }
 }

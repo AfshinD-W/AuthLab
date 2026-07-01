a56426643ad1e686e59d00da52885c0b842df772
jafari <afshinaa705@gmail.com>
2026-07-01 10:37:30 +0330

    Create AuthController

---
+++ AuthLab.Api/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using AuthLab.Api.Response;
+using AuthLab.Application.DTO.Login;
+using AuthLab.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthLab.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginRequestDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+
+            ApiResponse<LoginResponseDto> response = new()
+            {
+                Success = true,
+                Message = "User created successfully",
+                Data = result,
+            };
+
+            return Ok(response);
+        }
+    }
+}

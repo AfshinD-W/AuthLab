30b63d433ed20d3bcdf204e127bc9e47ab60a732
jafari <afshinaa705@gmail.com>
2026-07-01 12:35:32 +0330

    Develop first RefreshToken

--- AuthLab.Infrastructure/Identity/Services/AuthService.cs
+++ AuthLab.Infrastructure/Identity/Services/AuthService.cs
@@ -5,6 +5,7 @@ using AuthLab.Application.Interfaces;
 using AuthLab.Infrastructure.Database;
 using AuthLab.Infrastructure.Identity.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 
 namespace AuthLab.Infrastructure.Identity.Services
@@ -45,14 +46,7 @@ namespace AuthLab.Infrastructure.Identity.Services
 
             var jwtResponse = _jwtService.GenerateAccessToken(userInfo);
 
-            RefreshToken refreshToken = new()
-            {
-                Id = Guid.NewGuid(),
-                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
-                CreatedAt = DateTime.UtcNow,
-                ExpiresAt = DateTime.UtcNow.AddDays(15),
-                UserId = user.Id,
-            };
+            RefreshToken refreshToken = CreateRefreshToken(user.Id);
 
             await _appDbContext.RefreshTokens.AddAsync(refreshToken);
             await _appDbContext.SaveChangesAsync();
@@ -62,6 +56,51 @@ namespace AuthLab.Infrastructure.Identity.Services
                 AccessToken = jwtResponse.Token,
                 RefreshToken = refreshToken.Token,
                 ExpiresAt = jwtResponse.ExpiresAt,
+            };
+        }
+
+        public async Task<LoginResponseDto> RefreshTokenAsync(string refreshToken)
+        {
+            RefreshToken refresh = await _appDbContext.RefreshTokens.Include(r => r.User).SingleOrDefaultAsync(r => r.Token == refreshToken) ?? throw new NotFoundException("RefreshToken not found.");
+
+            if (refresh.IsRevoked || refresh.IsExpired)
+                throw new NotFoundException("RefreshToken is expierd.");
+
+            var userRoles = await _userManager.GetRolesAsync(refresh.User);
+
+            JwtUserInfoDto userInfo = new()
+            {
+                UserId = refresh.User.Id,
+                Email = refresh.User.Email!,
+                Roles = userRoles,
+            };
+
+            var jwtResponse = _jwtService.GenerateAccessToken(userInfo);
+
+            RefreshToken newRefreshToken = CreateRefreshToken(refresh.UserId);
+
+            refresh.RevokedAt = DateTime.UtcNow;
+            refresh.ReplacedByToken = newRefreshToken.Token;
+
+            await _appDbContext.RefreshTokens.AddAsync(newRefreshToken);
+            await _appDbContext.SaveChangesAsync();
+
+            return new LoginResponseDto()
+            {
+                AccessToken = jwtResponse.Token,
+                RefreshToken = newRefreshToken.Token,
+                ExpiresAt = jwtResponse.ExpiresAt,
+            };
+        }
+
+        private static RefreshToken CreateRefreshToken(string userId)
+        {
+            return new()
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                CreatedAt = DateTime.UtcNow,
+                ExpiresAt = DateTime.UtcNow.AddDays(15),
+                UserId = userId
             };
         }
     }

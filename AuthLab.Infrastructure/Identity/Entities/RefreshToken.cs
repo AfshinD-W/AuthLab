e4b320735f2d5d3f0200cf53d8d0082c35aab9f8
jafari <afshinaa705@gmail.com>
2026-07-01 11:08:21 +0330

    Create first RefreshToken entity

---
+++ AuthLab.Infrastructure/Identity/Entities/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthLab.Infrastructure.Identity.Entities
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; }
+
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime ExpiresAt { get; set; }
+
+        public DateTime? RevokedAt { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+
+        public User User { get; set; } = default!;
+    }
+}

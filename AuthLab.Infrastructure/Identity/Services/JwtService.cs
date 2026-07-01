b3cc5813a5f94225a37b732189111c8bdbd92211
jafari <afshinaa705@gmail.com>
2026-07-01 10:10:08 +0330

    Develop GenerateAccessToken method

--- AuthLab.Infrastructure/Identity/Services/JwtService.cs
+++ AuthLab.Infrastructure/Identity/Services/JwtService.cs
@@ -1,6 +1,10 @@
 ﻿using AuthLab.Application.DTO.Jwt;
 using AuthLab.Application.Interfaces;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace AuthLab.Infrastructure.Identity.Services
 {
@@ -14,7 +18,29 @@ namespace AuthLab.Infrastructure.Identity.Services
 
         public string GenerateAccessToken(JwtUserInfoDto userInfo)
         {
-            throw new NotImplementedException();
+            List<Claim> claims =
+                [
+                    new(JwtRegisteredClaimNames.Sub, userInfo.UserId),
+                    new(JwtRegisteredClaimNames.Email, userInfo.Email),
+                    new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                ];
+
+            claims.AddRange(userInfo.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            string secretKey = _jwtOptions.SecretKey;
+
+            SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(secretKey));
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.Sha256);
+
+            JwtSecurityToken token = new(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtOptions.ExpireMinutes),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }

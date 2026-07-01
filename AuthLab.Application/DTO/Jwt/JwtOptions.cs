9f0ef33b798586e7478321a82bfc5e3af81d7484
jafari <afshinaa705@gmail.com>
2026-07-01 10:10:25 +0330

    Config JwtOptions

---
+++ AuthLab.Application/DTO/Jwt/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthLab.Application.DTO.Jwt
+{
+    public class JwtOptions
+    {
+        public required string SecretKey { get; init; }
+        public required string Issuer { get; init; }
+        public required string Audience { get; init; }
+        public int ExpireMinutes { get; init; }
+    }
+}

76c4f48978c8239cb8c89c2905d530949ac1ef9f
jafari <afshinaa705@gmail.com>
2026-07-11 11:47:45 +0330

    Create password DTOs

---
+++ AuthLab.Application/DTO/Password/ForgotPasswordRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthLab.Application.DTO.Password
+{
+    public class ForgotPasswordRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ AuthLab.Application/DTO/Password/ResetPasswordRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthLab.Application.DTO.Password
+{
+    public class ResetPasswordRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}

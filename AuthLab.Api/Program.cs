e5b102a8510e1a2f4a4f1f51803594117c7b5c9f
jafari <afshinaa705@gmail.com>
2026-07-11 10:58:04 +0330

    Add AddAuthentication & AddJwtBearer to program.cs

--- AuthLab.Api/Program.cs
+++ AuthLab.Api/Program.cs
@@ -5,8 +5,11 @@ using AuthLab.Infrastructure.Database;
 using AuthLab.Infrastructure.Identity.Entities;
 using AuthLab.Infrastructure.Identity.Services;
 using AuthLab.Infrastructure.Identity.Validators;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +31,33 @@ builder.Services.AddIdentity<User, Role>(c =>
 //Configs
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
 builder.Services.Configure<RefreshTokenOptions>(builder.Configuration.GetSection("RefreshTokenOptions"));
+
+
+//Authentication
+JwtOptions jwtOptions = builder.Configuration.GetSection("Jwt").Get<JwtOptions>()!;
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtOptions.Issuer,
+            ValidAudience = jwtOptions.Audience,
+
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 //Services
 builder.Services.AddScoped<IUserService, UserService>();
